159e50d0c9978280730b8c36794286f9a135769c
cecile.crochon <cecile.crochon@progress-it.fr>
2020-12-14 12:05:00 +0100

    add random and greedy

--- knapsack.c
+++ knapsack.c
@@ -25,6 +25,10 @@ int meta(Item *tab, int n, long int b) {
   printf("Poids max : %d\n", b);
   printf("Poids sac : %d\n", calculpoids(tab, n, objSelect));
   
+  int better[n];
+  razObjSelect(objSelect, n);
+  greedy(tab,n,b,objSelect,better);
+
   return 0;
 }
 
@@ -34,6 +38,13 @@ void razObjSelect(int *objSelect, int n) {
     objSelect[i] = 0;
   }
  } 
+
+/* Remise à zéro de better */
+void razBetter(int *better, int n) {
+  for(int i; i < n; i++) {
+    better[i] = 0;
+  }
+}
 
 /* Sélectionne les 1ers objets d'un groupe tant qu'on ne dépasse pas le poids du sac */
 void initialisation(Item *tab, int n, long int b, int *objSelect) {
@@ -100,4 +111,78 @@ int calculpoids(Item *tab, int n, int *objSelect) {
     }
   }
   return weight;
+}
+
+/* Sélectionne des objets au hasard */
+void selectRandom(Item *tab, int n, long int b, int *objSelect) {
+  long int currentCapacity = 0;
+  if(b > 0) {
+    for(int i = 0; i < n; i++){
+      int rd = rand() % 3;
+      if((currentCapacity + tab[i].w[rd]) <= b) {
+        currentCapacity += tab[i].w[rd];
+        int index = (i * 3) + rd;
+        objSelect[index] = 1;
+      }
+    }
+  }
+}
+
+/* Ordonne les en fonction du meilleur rapport poid prix de chaque groupe d'objet */
+void orderBetter(Item *tab, int n, int *better) {
+  for(int i = 0; i < n; i++) {
+    int best = bestOne(tab[i]); //On sélectionne le meilleur objet parmis chaque groupe
+    better[i] = (i * 3) + best; // Si le meilleur objet du groupe n°1 est le 2eme alors son index est (1 * 3) + 2 = 5
+  }
+  for(int i = 0; i < n - 1; i++){
+    for(int j = i + 1; j < n; j++) {
+      if(rapportPrixPoids(tab, i) < rapportPrixPoids(tab, j)) {
+        int c = better[i];
+        better[i] = better[j];
+        better[j] = c;
+      }
+    } 
+  }
+}
+
+/* Retourne le rapport prix-poids d'un objet */
+float rapportPrixPoids(Item *tab, int index) {
+  return (getPoids(tab,index) / getPrix(tab,index));
+}
+
+/* Retourne le quotient d'une division euclidenne */
+int quotient(int dividende, int diviseur) {
+  int quotient = (dividende - reste(dividende, diviseur)) / diviseur;
+  return quotient; 
+}
+
+/* Retourne le reste d'une division euclidienne*/
+int reste (int dividende, int diviseur) {
+  return dividende % diviseur;
+}
+
+/* Retourne le poids d'un objet*/
+int getPoids(Item *tab, int index){
+  int indexObj = reste(index, 3);
+  int indexTab = quotient(index, 3);
+  return tab[indexTab].w[indexObj];
+}
+
+/* Retourne le prix d'un objet */
+int getPrix(Item *tab, int index) {
+  int indexObj = reste(index, 3);
+  int indexTab = quotient(index, 3);
+  return tab[indexTab].p[indexObj];
+}
+
+/* Algorithme glouton*/
+void greedy(Item *tab, int n, long int b, int *objSelect, int *better) {
+  orderBetter(tab,n,better);
+  long int currentCapacity =  0;
+  for(int i = 0; i < n; i++) {
+    if(currentCapacity + getPoids(tab,i) <= b) {
+      currentCapacity += getPoids(tab,i);
+      objSelect[i] = 1;
+    }
+  }
 }
